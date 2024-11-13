78571f338a01a83ea670c4502bb211fb74ea9fc3
Nicol04 <katherin.nicol04@gmail.com>
2024-11-13 02:11:46 -0500

    2do commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,57 +1,37 @@
 <?php
-
-/**
- * Created by Reliese Model.
- */
-
 namespace App\Models;
 
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-
-/**
- * Class User
- * 
- * @property int $id
- * @property int|null $role_id
- * @property string $name
- * @property string $email
- * @property Carbon|null $email_verified_at
- * @property string $password
- * @property string|null $remember_token
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- * 
- * @property Role|null $role
- * @property Collection|CarteraCliente[] $cartera_clientes
- * @property Collection|HistorialCobro[] $historial_cobros
- * @property Collection|Vehiculo[] $vehiculos
- *
- * @package App\Models
- */
-class User extends Model
+use Illuminate\Foundation\Auth\User as Authenticatable; 
+use Illuminate\Notifications\Notifiable;
+class User extends Authenticatable
 {
 	protected $table = 'users';
-
-	protected $casts = [
-		'role_id' => 'int',
-		'email_verified_at' => 'datetime'
-	];
-
-	protected $hidden = [
-		'password',
-		'remember_token'
-	];
-
+	protected $primaryKey = 'id';
+	use Notifiable;
 	protected $fillable = [
 		'role_id',
 		'name',
 		'email',
-		'email_verified_at',
 		'password',
-		'remember_token'
 	];
+	protected $hidden = [
+		'password',
+		'remember_token',
+	];
+	protected $casts = [
+		'role_id' => 'int',
+		'email_verified_at' => 'datetime',
+		'password' => 'hashed'
+	];
+	protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 
 	public function role()
 	{
